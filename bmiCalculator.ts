5589dc4e3e2f1d1024ba16b704ecbb690504fad6
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-04-24 06:46:23 +0530

    ex 9.1-9.3 done

---
+++ bmiCalculator.ts
@@ -0,0 +1,37 @@
+interface ArgsObject {
+	value1: number,
+	value2: number
+}
+
+const parseArgs = (args: Array<string>): ArgsObject => {
+	if (args.length > 4) throw new Error(`Too many arguments`);
+	if (args.length < 4) throw new Error('Not enough arguments');
+
+	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+		return {
+			value1: Number(args[2]),
+			value2: Number(args[3])
+		}
+	} else {
+		throw new Error('Provided values were not numbers!');
+	}
+}
+
+
+const calculateBmi = (a: number, b: number): string => {
+	const bmi = (b * 100 * 100) / (a * a);
+	if (bmi < 18.5) {
+		return 'Underweight';
+	} else if (bmi > 18.5 && bmi < 25) {
+		return 'Normal (healthy weight)';
+	} else if (bmi > 25) {
+		return 'Overweight';
+	}
+};
+
+try {
+	const { value1, value2 } = parseArgs(process.argv);
+	console.log(calculateBmi(value1, value2));
+} catch (error) {
+	console.log('Error, something bad happened, message: ', error.message);
+}
---
+++ exerciseCalculator.ts
@@ -0,0 +1,68 @@
+interface ArgsInterface {
+	value1: Array<number>,
+	value2: number
+}
+
+const parseArguments = (args: Array<string>): ArgsInterface => {
+	if (args.length < 3) throw new Error('Not enough arguments');
+	const newArray = args.slice(3);
+	const numberArray = newArray.map(item => {
+		if (!isNaN(Number(item))) {
+			return Number(item)
+		} else {
+			throw new Error('Providied values were not numbers!')
+		}
+	});
+
+	if (!isNaN(Number(args[2]))) {
+		return {
+			value1: numberArray,
+			value2: Number(args[2])
+		}
+	} else {
+		throw new Error('Provided values were not numbers!');
+	}
+}
+
+
+interface Result {
+	periodLength: number,
+	trainingDays: number,
+	success: boolean,
+	rating: number,
+	ratingDescription: string,
+	target: number,
+	average: number
+}
+
+const calculateExercises = (arr: Array<number>, num: number): Result => {
+	const periodLength = arr.length;
+	const trainingDays = arr.filter(val => val !== 0).length;
+	const average = arr.reduce((acc, val) => acc + val, 0) / periodLength;
+	let success;
+	if (average > num) {
+		success = true;
+	} else {
+		success = false;
+	}
+	const rating = 2;
+	const ratingDescription = 'not too bad but could be better';
+	const target = num;
+
+	return {
+		periodLength,
+		trainingDays,
+		success,
+		rating,
+		ratingDescription,
+		target,
+		average
+	}
+}
+
+try {
+	const { value1, value2 } = parseArguments(process.argv);
+	console.log(calculateExercises(value1, value2));
+} catch (error) {
+	console.log('Error, something bad happened, message: ', error.message);
+}
